4e6149d636adbf5c5359f2fb9e9bb3bc79fde5ee
Sam Rossilli <sarossilli@gmail.com>
2022-06-05 09:48:02 -0500

    Added basic CSS

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
-import {data} from "../DummyData";
-import ProjectCard from "../src/components/ProjectCard"
+import { data } from "../DummyData";
+import ProjectCard from "../src/components/ProjectCard";
 const index = () => {
-    console.log(data)
-    return(
-        <div>
-            <h1>Sarossilli</h1>
-            {data.map(function(p){
-                return (<ProjectCard project={p}></ProjectCard>);
-            })}
+  console.log(data);
+  return (
+    <div>
+      <h1>Sarossilli</h1>
+      <div className="container projects-list">
+        <div className="row">
+          {data.map(function (p) {
+            return <ProjectCard project={p}></ProjectCard>;
+          })}
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 export default index;
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,21 +1,31 @@
-const ProjectCard = ({project}) => {
-    
-    return (
-        <div className="col-xs-12 col-sm-12 col-md-6 col-lg4 col-sm-12 col-xs-12">
-            <div className="card card-height p-4 mt-4">
-            <h3>{project.title}</h3>
+const ProjectCard = ({ project }) => {
+  return (
+    <div className="col-xs-12 col-sm-12 col-md-6 col-lg4 col-sm-12 col-xs-12">
+      <div className="card card-height p-4 mt-4">
+        <div className="project-info">
+          <div className="d-flex justify-content-between mb-3">
+            <h3 className="text-truncate w-200">{project.title}</h3>
+          </div>
+          <img
+            src="https://raw.githubusercontent.com/Sarossilli/Musical-Joycons/master/readme/header.gif"
+            className="project-img d-flex flex0row justify-content-center alighn-items-center h-200"
+          ></img>
+          <div>
             <p>{project.smallDesc}</p>
-            <ul>
-                {project.skills.map(function(skill){
-                    return(<li>{skill}</li>);
-                })}
-            </ul>
-
-            </div>
-
+          </div>
         </div>
-    );
 
-}
+        <div className="card skills">
+          <div class="card-header">Skills</div>
+          <ul>
+            {project.skills.map(function (skill) {
+              return <li>{skill}</li>;
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default ProjectCard;
