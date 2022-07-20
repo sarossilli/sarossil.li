56834d6ba0bc0e86ba7112c563bf8c386427eb4b
Sam Rossilli <sarossilli@gmail.com>
2022-07-19 21:40:18 -0500

    update configurations

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,6 @@
 import ProjectCardList from "../src/components/ProjectCardList";
 import Navbar from "../src/components/Navbar";
 
-import Amplify, { Auth, API } from "aws-amplify";
-import awsconfig from "../src/aws-exports";
-Amplify.configure(awsconfig);
-
 const index = () => {
   return (
     <div>
--- src/components/ProjectCardList.jsx
+++ src/components/ProjectCardList.jsx
@@ -1,19 +1,11 @@
 import ProjectCard from "./ProjectCard";
 
-import { API } from "aws-amplify";
-
-import { listProjects } from "../graphql/queries";
 import { useState } from "react";
 
 function ProjectCardList() {
   const [projectList, setProjectList] = useState({});
   const [isLoaded, setLoadedState] = useState(false);
-
-  API.graphql({ query: listProjects }).then(function (result) {
-    setProjectList(result.data.listProjects.items);
-    setLoadedState(true);
-  });
-
+  
   if (isLoaded) {
     return (
       <div className="container projects-list">
